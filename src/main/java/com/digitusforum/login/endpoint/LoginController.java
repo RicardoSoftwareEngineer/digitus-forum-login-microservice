165993590cb99ce24d77151848223052ef79cfa4
Ricardo Ribeiro <riceirolves@gmail.com>
2021-06-26 16:15:53 -0300

    C:/Program Files/Git/login/v1/login/by/emailAndPassword endpoint created

---
+++ src/main/java/com/digitusforum/login/endpoint/LoginController.java
@@ -0,0 +1,20 @@
+package com.digitusforum.login.endpoint;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import vo.UserVO;
+
+
+@RestController
+public class LoginController {
+	
+	@PostMapping(value = "/login/v1/login/by/emailAndPassword")
+    public Object loginByEmailAndPassword(@RequestHeader(defaultValue = "en_us") String locale, @RequestBody UserVO userVO) {
+		
+		return "";
+	}
+
+}
