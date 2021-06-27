c267a8bcd440de525ec024215ab9d92807086ebc
Ricardo Ribeiro <riceirolves@gmail.com>
2021-06-27 00:55:02 -0300

    login controller now is the only microservice able to create token

--- src/main/java/com/digitusforum/login/endpoint/LoginController.java
+++ src/main/java/com/digitusforum/login/endpoint/LoginController.java
@@ -1,22 +1,34 @@
 package com.digitusforum.login.endpoint;
 
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAmount;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.digitusforum.login.service.TokenService;
+
+import microservice.UserMicroservice;
+import service.TimeService;
 import vo.TokenVO;
 import vo.UserVO;
 
 
 @RestController
 public class LoginController {
+	UserMicroservice userMicroservice = new UserMicroservice();
+	TokenService TokenService = new TokenService();
 	
-	@PostMapping(value = "/login/v1/loginByEmailAndPassword")
+	@RequestMapping(value = "/login/v1/loginByEmailAndPassword")
     public Object loginByEmailAndPassword(@RequestHeader(defaultValue = "en_us") String locale, @RequestBody UserVO userVO) {
-		UserVO user = new UserVO();
-		user.setName("sup bro aihusiuahsui");
-		return user;
+		userVO = userMicroservice.checkEmailAndPassword(userVO, locale);
+		String token = TokenService.createJWTToken(ZonedDateTime.now().plus(1, ChronoUnit.MINUTES), userVO);
+		
+		return new TokenVO();
 	}
 
 }
