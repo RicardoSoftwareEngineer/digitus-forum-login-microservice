c25755d8ec833d2934dca5366495d5eea146d977
Ricardo Ribeiro <riceirolves@gmail.com>
2021-06-27 10:28:22 -0300

    cleanupp

--- src/main/java/com/digitusforum/login/endpoint/LoginController.java
+++ src/main/java/com/digitusforum/login/endpoint/LoginController.java
@@ -2,9 +2,7 @@ package com.digitusforum.login.endpoint;
 
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAmount;
 
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,21 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 import com.digitusforum.login.service.TokenService;
 
 import microservice.UserMicroservice;
-import service.TimeService;
 import vo.TokenVO;
 import vo.UserVO;
-
 
 @RestController
 public class LoginController {
 	UserMicroservice userMicroservice = new UserMicroservice();
-	TokenService TokenService = new TokenService();
-	
+
 	@RequestMapping(value = "/login/v1/loginByEmailAndPassword")
-    public Object loginByEmailAndPassword(@RequestHeader(defaultValue = "en_us") String locale, @RequestBody UserVO userVO) {
+	public Object loginByEmailAndPassword(@RequestHeader(defaultValue = "en_us") String locale,
+			@RequestBody UserVO userVO) {
 		userVO = userMicroservice.checkEmailAndPassword(userVO, locale);
 		String token = TokenService.createJWTToken(ZonedDateTime.now().plus(1, ChronoUnit.MINUTES), userVO);
-		
+
 		return new TokenVO();
 	}
 
