d6e6ee6cfdd2311739df46919c68211ed9dafda3
Ricardo Ribeiro <riceirolves@gmail.com>
2021-06-27 10:49:16 -0300

    cleanup

--- src/main/java/com/digitusforum/login/endpoint/HealthCheckController.java
+++ src/main/java/com/digitusforum/login/endpoint/HealthCheckController.java
@@ -2,11 +2,9 @@ package com.digitusforum.login.endpoint;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
 @RestController
 public class HealthCheckController {
 
