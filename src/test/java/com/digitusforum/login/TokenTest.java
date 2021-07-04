40a6b878fd7095504be5c78069aa7821103b4887
Ricardo Ribeiro <riceirolves@gmail.com>
2021-07-04 18:29:08 -0300

    dependecy injection added for a more decoupled environment

--- src/test/java/com/digitusforum/login/TokenTest.java
+++ src/test/java/com/digitusforum/login/TokenTest.java
@@ -6,9 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.time.ZonedDateTime;
-import java.time.temporal.ChronoUnit;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -19,6 +16,7 @@ import com.digitusforum.login.service.TokenService;
 
 import microservice.UserMicroservice;
 import service.EnvironmentService;
+import service.RequestService;
 import vo.UserVO;
 
 @SpringBootTest
@@ -28,6 +26,9 @@ public class TokenTest {
 
 	@BeforeAll
 	public static void setupMock() {
+		RequestService requestService = mock(RequestService.class);
+		when(requestService.isUp(Mockito.anyString())).thenReturn(true);
+
 		userMicroservice = mock(UserMicroservice.class);
 		when(userMicroservice.checkEmailAndPassword(Mockito.any(UserVO.class), Mockito.anyString()))
 				.thenReturn(new UserVO(2, "ricardo", "ricardo@gmail.com", "password"));
