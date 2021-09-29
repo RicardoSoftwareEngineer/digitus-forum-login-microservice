63269c1a1abbbf8ca849e31fc9d5202b59c8ced8
Ribeiro Gonçalves <roig@gft.com>
2021-09-29 09:54:33 -0300

    package reorganization

--- src/main/java/com/digitusforum/login/util/ShutdownController.java
+++ src/main/java/com/digitusforum/login/util/ShutdownController.java
@@ -1,4 +1,4 @@
-package com.digitusforum.login.endpoint;
+package com.digitusforum.login.util;
 
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
